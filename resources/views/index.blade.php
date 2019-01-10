5d0c0539a1769718405abe31fca7934dd5d63ce2
Keith Mathur <keith.mathur@dcmail.ca>
2019-01-10 18:33:21 -0500

    Initial bootstrap is in. Headers, footers, css, and layout files setup. Homepage is not complete.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section ('content')
+
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
+            <h1 class="display-4 font-weight-normal">MoDoVation</h1>
+            <p class="lead font-weight-normal">Create a plan to achieve your goals. Stay productive, and work hard every day.</p>
+            <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+        </div>
+    </div>
+
+@endsection
