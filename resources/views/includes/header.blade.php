fa971bac5fccd2b67b3c73c6d7e8d8364328e15d
Liam <liam.stachiw@gmail.com>
2019-01-22 15:49:34 -0500

    Added Logo and Icon
    Added a logo to the header and footer that should appear on every page. I also added an icon that will appear on the tab for the website. There are currently 2 versions of the logo/icon with slightly different colour schemes that we can decide on later.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -8,6 +8,7 @@
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v3.8.5">
     <title>MoDoVation</title>
+    <link rel="icon" href="/images/MDVIcon.ico"/>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
@@ -40,9 +41,7 @@
 
       <nav class="site-header sticky-top py-1">
           <div class="container d-flex flex-column flex-md-row justify-content-between">
-            <a class="py-2" href="/">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto" focusable="false" role="img"><title>Product</title><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
-            </a>
+            <a class="py-2" href="/"><img src="/images/MDVLogo.png" alt="Logo" width="66" height="24"></a>
             <a class="py-2 d-none d-md-inline-block" href="/todo">To Do List</a>
             <a class="py-2 d-none d-md-inline-block" href="/goals">Goals</a>
             <a class="py-2 d-none d-md-inline-block" href="/journals">Journals</a>
