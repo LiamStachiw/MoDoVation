e0bfcfd3d124f8df54be4ebd9719d21fc20489f1
Liam <liam.stachiw@gmail.com>
2019-03-22 15:15:52 -0400

    Small CSS change
    Just changed the resizing checkboxes CSS so that it only affects the checkboxes for tasks

--- resources/views/To Do List/taskEntry.blade.php
+++ resources/views/To Do List/taskEntry.blade.php
@@ -2,7 +2,7 @@
         <a style="float: left; display: inline" href="/task/{{ $task->id }} ">
             {{ $task->taskName }} 
         </a> 
-        <input style="float: right; display: inline" id="{{ $task->id }}" data-goalId="{{ $task->goal_id }}" class="taskCheckbox" type="checkbox" value="{{ $task->taskName }}" 
+        <input style="float: right; display: inline" id="{{ $task->id }}" data-goalId="{{ $task->goal_id }}" class="taskCheckbox resize-check" type="checkbox" value="{{ $task->taskName }}" 
             @if($task->isComplete == 1) checked @endif
             @if($task->isDisabled == 1) disabled @endif> 
     </div>
